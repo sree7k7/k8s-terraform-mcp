fd265a60f34c296dc3fd1e1e26b4a078c2888ef2
Srikanth <sree7k7@gmail.com>
2026-03-01 15:10:06 +0100

    Added: EKS cluster permission to aws-to-github role

--- main.tf
+++ main.tf
@@ -124,19 +124,19 @@ module "eks" {
   }
 
   # Grant your local AWS user admin access
-  # access_entries = {
-  #   sran_nice = {
-  #     principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/sran-nice"
-  #     policy_associations = {
-  #       admin = {
-  #         policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  #         access_scope = {
-  #           type = "cluster"
-  #         }
-  #       }
-  #     }
-  #   }
-  # }
+  access_entries = {
+    sran_nice = {
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-to-github"
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
 }
 
 
